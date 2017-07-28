d6733d4ffc4bf829270ef8f8c931ca79d3571cf1
zhanggaohao <zhanggaohao@hotmail.com>
2017-07-28 23:01:06 +0800

    sky

---
+++ sky-web/src/main/java/com/sky/web/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.sky.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @annotation TODO
+ * @version 1.0
+ * @author zhanggaohao
+ * @date 2017年7月15日 下午9:17:20
+ *
+ */
+@Controller
+public class IndexController {
+
+	private Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+	@RequestMapping("/index")
+	public ModelAndView main(){
+		logger.info("into the testController.");
+		return new ModelAndView("index");
+	}
+	
+}
